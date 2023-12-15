422f88025adbf936f314b40e32e2a120b27f0204
Alina-Goova <147145257+Alina-Goova@users.noreply.github.com>
2023-12-15 13:36:27 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,4 @@
+from paket.csv1 import load_table_csv, save_table_csv
+from paket.pickle1 import load_table_pickle, save_table_pickle
+from paket.txt1 import save_table_txt, print_table_txt
+from paket.basa_oper import *
